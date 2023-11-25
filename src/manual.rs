982ad00cb8da9c56290cb5ced5e7e3347c335b87
KENZ <KENZ.gelsoft@gmail.com>
2023-11-25 12:56:23 +0900

    Rewrite B_MOUSE_BUTTON() macro to const func.

--- src/manual.rs
+++ src/manual.rs
@@ -6,5 +6,9 @@
 use std::os::raw::c_int;
 
 pub const fn _rule_(t: c_int, l: c_int, b: c_int, r: c_int) -> c_int {
-	(t << 12) | (l << 8) | (b << 4) | r
+    (t << 12) | (l << 8) | (b << 4) | r
+}
+
+pub const fn B_MOUSE_BUTTON(v: c_int) -> c_int {
+    1 << (v - 1)
 }
