d644ffe970b70c8121256a47b9eb1606f8274658
KENZ <KENZ.gelsoft@gmail.com>
2023-11-25 12:59:32 +0900

    Rewrite B_TO_POSIX_ERROR() macro to const func.

--- src/manual.rs
+++ src/manual.rs
@@ -9,6 +9,9 @@ pub const fn _rule_(t: c_int, l: c_int, b: c_int, r: c_int) -> c_int {
     (t << 12) | (l << 8) | (b << 4) | r
 }
 
+pub const fn B_TO_POSIX_ERROR(v: c_int) -> c_int {
+    v
+}
 pub const fn B_FROM_POSIX_ERROR(v: c_int) -> c_int {
     v
 }
