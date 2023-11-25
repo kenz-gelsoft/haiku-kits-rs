76ede58ca924e7eaa6e8b9a86f482324682e1b6e
KENZ <KENZ.gelsoft@gmail.com>
2023-11-25 12:58:16 +0900

    Rewrite B_FROM_POSIX_ERROR() macro to const func.

--- src/manual.rs
+++ src/manual.rs
@@ -9,6 +9,10 @@ pub const fn _rule_(t: c_int, l: c_int, b: c_int, r: c_int) -> c_int {
     (t << 12) | (l << 8) | (b << 4) | r
 }
 
+pub const fn B_FROM_POSIX_ERROR(v: c_int) -> c_int {
+    v
+}
+
 pub const fn B_MOUSE_BUTTON(v: c_int) -> c_int {
     1 << (v - 1)
 }
